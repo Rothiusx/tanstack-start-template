7a2f8856e9286ab77ab71c05737fc1b02a4bd1fb
Adam Molin <Rothiusx@seznam.cz>
2025-03-19 22:31:42 +0100

    Change language handling when formatting dates

--- src/components/common/format-date.tsx
+++ src/components/common/format-date.tsx
@@ -1,25 +1,17 @@
-import { formatDate } from '@/lib/utils'
+import { useLocale } from '@/hooks/use-locale'
 import { useEffect, useState } from 'react'
 
 // Component to handle date display with proper hydration
 export function FormatDate({ date }: { date: Date | string | number }) {
-  // For client, we'll update after hydration
   const [clientDate, setClientDate] = useState<string>(date.toString())
+  const language = useLocale()
 
-  // After hydration is complete, we can update to client timezone if needed
   useEffect(() => {
-    // Only run this code on the client after hydration
     if (typeof window !== 'undefined') {
-      // const clientFormattedDate = new Intl.DateTimeFormat(navigator.language, {
-      //   dateStyle: 'medium',
-      //   timeStyle: 'short',
-      // }).format(new Date(date))
       console.log('clientFormattedDate', clientDate)
-      setClientDate(date.toLocaleString())
+      setClientDate(date.toLocaleString(language))
     }
-  }, [clientDate, date])
+  }, [clientDate, date, language])
 
-  // This ensures we always render exactly what the server rendered initially
-  // Then switch to client-formatted date after hydration
-  return <span suppressHydrationWarning>{date.toLocaleString()}</span>
+  return <span suppressHydrationWarning>{clientDate}</span>
 }
