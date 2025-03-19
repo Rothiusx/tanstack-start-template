e1e8556ca5a7df8218576c030c64709ae841da34
Adam Molin <Rothiusx@seznam.cz>
2025-03-19 22:43:08 +0100

    Changed timezone render in format date component

--- src/components/common/format-date.tsx
+++ src/components/common/format-date.tsx
@@ -1,19 +1,21 @@
-import { useLocale } from '@/hooks/use-locale'
+import { formatDate, getBrowserLocale } from '@/lib/utils'
 import { useRouteContext } from '@tanstack/react-router'
 import { useEffect, useState } from 'react'
 
 // Component to handle date display with proper hydration
 export function FormatDate({ date }: { date: Date | string | number }) {
-  const [clientDate, setClientDate] = useState<string>(date.toString())
   const language = useRouteContext({
     from: '__root__',
     select: ({ user }) => user?.language,
   })
+  const [clientDate, setClientDate] = useState<string>(
+    date.toLocaleString(language),
+  )
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       console.log('clientFormattedDate', clientDate)
-      setClientDate(date.toLocaleString(language))
+      setClientDate(date.toLocaleString(language ?? getBrowserLocale()))
     }
   }, [clientDate, date, language])
 
